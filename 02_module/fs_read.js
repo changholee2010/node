ca9f795d5ab2a57f433fc83faf14866e7d779e86
chichi <cyannara@naver.com>
2025-08-25 16:52:54 +0900

    express 설치

---
+++ 02_module/fs_read.js
@@ -0,0 +1,16 @@
+// fs_read.js
+const fs = require("fs");
+
+// 비동기(non블로킹)/동기(블로킹)
+fs.readFile("./stdout.log", "utf8", (err, buf) => {
+  // callback함수.
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(buf);
+});
+let errBuf = fs.readFileSync("./stderr.log", "utf8");
+console.log(errBuf);
+
+console.log("다른코드");
