f342dd718edbc5fe7a9249daa49fc8f4782d53b6
chichi <cyannara@naver.com>
2025-08-27 10:24:37 +0900

    nodemon

--- 04_mysql/app.js
+++ 04_mysql/app.js
@@ -1,17 +1,7 @@
 // app.js
 const express = require("express");
-const mysql = require("mysql2");
 const parser = require("body-parser");
-
-// connect pool 생성.
-const pool = mysql.createPool({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "dev01",
-  password: "dev01",
-  database: "dev",
-  connectionLimit: 10,
-});
+const sql = require("./sql");
 
 const app = express();
 app.use(parser.urlencoded()); // x-www-form-urlencoded
@@ -22,59 +12,42 @@ app.get("/", (req, resp) => {
 });
 
 // 고객목록.
-app.get("/customers", (req, resp) => {
-  pool.getConnection((err, connection) => {
-    // getConnection => connection 객체 획득.
-    if (err) {
-      console.log(err);
-      return;
-    }
-    connection.query("select * from customers", (err, results) => {
-      if (err) {
-        console.log(err);
-        resp.send("쿼리실행중 에러");
-        return;
-      }
-      console.log(results);
-      // resp.send("실행완료.");
-      resp.json(results);
-      connection.release(); // connection => pool 환원.
-    }); // end of query().
-  }); // end of getConnection().
+app.get("/customers", async (req, resp) => {
+  try {
+    let customerList = await sql.execute("select * from customers");
+    console.log(customerList);
+    resp.json(customerList);
+  } catch (err) {
+    console.log(err);
+    resp.json({ retCode: "Error" });
+  }
 });
 
 // 등록.
-app.post("/customer", (req, resp) => {
-  console.log(req.body.param);
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    connection.query(
-      "insert into customers set ?",
-      [req.body.param], // [{name:'방재우', email:'ban@email', phone:'010-11'}]
-      (err, results) => {
-        if (err) {
-          console.log(err);
-          resp.send("쿼리실행중 에러");
-          return;
-        }
-        console.log(results);
-        // resp.send("실행완료.");
-        resp.json(results);
-        connection.release(); // connection => pool 환원.
-      }
-    ); // end of query().
-  }); // end of getConnection().
+app.post("/customer", async (req, resp) => {
+  try {
+    let result = await sql.execute(
+      "insert into customers set ?", //
+      [req.body.param]
+    );
+    resp.json(result);
+  } catch (err) {
+    resp.json({ retCode: "Error" });
+  }
 });
 
 // http://localhost:8080/boardList.do?page=3
 // http://localhost:3000/customer/:id
-app.delete("/customer/:id", (req, resp) => {
-  console.dir(req.params);
-  //
-  resp.send("OK");
+app.delete("/customer/:id", async (req, resp) => {
+  try {
+    let result = await sql.execute(
+      "delete from customers where id = ?", //
+      [req.params.id]
+    );
+    resp.json(result);
+  } catch (err) {
+    resp.json({ retCode: "Error" });
+  }
 });
 
 app.listen(3000, () => {
